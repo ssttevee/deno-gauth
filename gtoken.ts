611b469acbb1ff0e339ebf617dd17a6ee9834988
Steve Lam <slam@akshonesports.com>
2022-11-29 16:34:28 -0800

    comment out unused properties

--- gtoken.ts
+++ gtoken.ts
@@ -4,16 +4,16 @@ import { fromPKCS8 } from "https://deno.land/x/jose@v4.10.4/runtime/asn1.ts";
 export type Scopes = string | string[];
 
 export interface Credentials {
-  type: string;
-  project_id: string;
-  private_key_id: string;
+  // type: string;
+  // project_id: string;
+  // private_key_id: string;
   private_key: string;
   client_email: string;
-  client_id: string;
-  auth_uri: string;
-  token_uri: string;
-  auth_provider_x509_cert_url: string;
-  client_x509_cert_url: string;
+  // client_id: string;
+  // auth_uri: string;
+  // token_uri: string;
+  // auth_provider_x509_cert_url: string;
+  // client_x509_cert_url: string;
 }
 
 interface TokenResponse {
